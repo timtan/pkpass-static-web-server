1ba2567e2895e1da28aad3b0488d655797a5ddd6
tim hsu <tim.yellow@gmail.com>
2013-01-12 02:50:53 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ long_description = (open('README.md').read()  )
 
 setup(
     name='PkpassStaticWebServer',
-    version='0.1', 
+    version='0.1.1', 
     url='https://github.com/timtan/pkpass-static-web-server',
     description='a static server can server pkpass file', 
     long_description=long_description,
     author='tim',
     author_email='tim.yellow@gmail.com',
+    py_modules=['pkpass_static_web_server']
     zip_safe=False,
     entry_points={
         'console_scripts': [
